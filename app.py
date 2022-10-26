9c5fab4e682a4d9f5cb1e538bd5a2d00dba54139
GabrielleWal <72016856+GabrielleWal@users.noreply.github.com>
2022-10-26 23:02:41 +0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from database import Database
 
 app = Flask(__name__)
 db = Database()
-api = Api(app)
 
 @app.get('/')
 def hello_world():
@@ -27,24 +26,6 @@ def login():
         password = request.json["password"]
     except KeyError:
         return 'Missing parameters'
-
-class Todos():
-    def post(self):
-        try:
-            user_id = request.json["user_id"]
-            content = request.json["content"]
-        except KeyError:
-            return 'Missing parameters'
-        return db.add_totos(user_id,content)
-
-    def get(self):
-        try:
-            user_id = request.json["user_id"]
-        except KeyError:
-            return 'Missing parameters'
-        return db.get_totos(user_id)
-
-api.add_ressource(Todos, "/todos")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
